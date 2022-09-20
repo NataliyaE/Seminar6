c27413304c5aceb5c0c8f7c2d184fde230e62166
Natali <salamatina_nata@mail.ru>
2022-09-20 09:28:36 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// пользователь вводит с клавиатуры М чисел. посчитать, сколько чисел больше нуля введено
+
+
+Console.WriteLine("Введите длину массива");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[length];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Кол-во элементов больше нуля = {FindCount(array)}");
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine("Введите элемент массива");
+        int arrayElement = Convert.ToInt32(Console.ReadLine());
+        array[i] = arrayElement;
+    }
+}
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write($"{array[index]}; ");
+        index ++;
+    }
+}
+
+int FindCount(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (array[i] > 0)
+       {
+            result ++;
+       }
+    }
+    return result;
+}
